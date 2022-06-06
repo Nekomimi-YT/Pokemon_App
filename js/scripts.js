0cb0d664657bef497500545496a71719495d77e9
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-06-06 11:42:39 +0200

    Initial commit of css, html and js to this repository
    css: empty
    
    js: simple alert(), variable declarations  and document.write() additions
    
    html: basic skeleton HTML with links added to css/styles.css and js/scripts/js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+alert('Hello Jenny!');
+alert('What happens if I do this again?');
+alert('Ok, no more.  I promise');
+
+let favoriteFood = 'Favorite food? That\'s a hard question to answer...';
+document.write(favoriteFood);
+favoriteFood = ' But variety is the spice of life...';
+document.write(favoriteFood);
+favoriteFood = ' And don\'t forget the dark chocolate!';
+document.write(favoriteFood);
