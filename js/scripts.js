a871c2f9894531e30b266fdd0390a9006f78db06
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-06-09 15:51:59 +0200

    Implement forEach loop on objects in array tradeList
    Converted arrays containing objects within main array tradeList to objects containing objects
    
    Changed loop implementation to write representative data to the DOM from for loop to a forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -12,88 +12,78 @@ money exchanged in both buy and sell transactions), and the top ten company tick
 
 // Current short list of representatives
 let tradeList = [
-  [
-    {name: 'Hon. Virginia Foxx'},
-    {state: 'North Carolina'},
-    {party: 'Republican'},
-    {tradeType: [
-      {'purchase': 220}, {'sale': 34}
-      ]
+  {
+    name: 'Hon. Virginia Foxx',
+    state: 'North Carolina',
+    party: 'Republican',
+    tradeType: {
+      purchase: 220, sale: 34
     },
-    {tradeVolume: '~$6,827,627.00'},
-    {topTenTickers: [
+    tradeVolume: '~$6,827,627.00',
+    topTenTickers: [
       'CWEN.A', ' OLP', ' T', ' PPL', ' BP', ' NHI', ' SO', ' DUK', ' BXMT', ' AM'
-      ]
-    }
-  ],
-  [
-    {name: 'Hon. Alan S. Lowenthal'},
-    {state: 'California'},
-    {party: 'Democrat'},
-    {tradeType: [
-      {'purchase': 260}, {'sale': 356}
-      ]
+    ]  
+  },
+  {
+    name: 'Hon. Alan S. Lowenthal',
+    state: 'California',
+    party: 'Democrat',
+    tradeType: {
+      purchase: 260, sale: 356
     },
-    {tradeVolume: '~$5,348,814.00'},
-    {topTenTickers: [
+    tradeVolume: '~$5,348,814.00',
+    topTenTickers: [
       'RUN', ' SH', ' PSQ', ' IWN', ' COLD', ' BSX', ' BKNG', ' GM', ' IFF', ' NFLX'
-      ]
-    }
-  ],
-  [
-    {name: 'Hon. Aston Donald McEachin'},
-    {state: 'Virginia'},
-    {party: 'Democrat'},
-    {tradeType: [
-      {'purchase': 6}, {'sale': 0}
-      ]
+    ]
+  },
+  {
+    name: 'Hon. Aston Donald McEachin',
+    state: 'Virginia',
+    party: 'Democrat',
+    tradeType: {
+      purchase: 0, sale: 0
     },
-    {tradeVolume: '~$48,003.00'},
-    {topTenTickers: [
+    tradeVolume: '~$48,003.00',
+    topTenTickers: [
       'RIVN', ' TXN', ' NXPI', ' ON', ' D', ' AXP'
-      ]
-    }
-  ],
-  [
-    {name: 'Hon. Austin Scott'},
-    {state: 'Georgia'},
-    {party: 'Republican'},
-    {tradeType: [
-      {'purchase': 40}, {'sale': 35}
-      ]
+    ]
+  },
+  {
+    name: 'Hon. Austin Scott',
+    state: 'Georgia',
+    party: 'Republican',
+    tradeType: {
+      purchase: 40, sale: 35
     },
-    {tradeVolume: '~$918,537.50'},
-    {topTenTickers: [
+    tradeVolume: '~$918,537.50',
+    topTenTickers: [
       'FCEL', ' PLUG', ' CLNE', ' BE', ' T', ' JNJ', ' KPLT', ' NVVE', ' BLDP', ' KPLTW'
-      ]
-    }
-  ],
-  [
-    {name: 'Hon. Thomas Suozzi'},
-    {state: 'New York'},
-    {party: 'Democrat'},
-    {tradeType: [
-      {purchase: 57}, {sale: 41}
-      ]
+    ]
+  },
+  {
+    name: 'Hon. Thomas Suozzi',
+    state: 'New York',
+    party: 'Democrat',
+    tradeType: {
+      purchase: 57, sale: 41
     },
-    {tradeVolume: '~$3,441,549.00'},
-    {topTenTickers: [
+    tradeVolume: '~$3,441,549.00',
+    topTenTickers: [
       'MS', ' APPL', ' DG', ' BBH', ' CG', ' PYPL', ' SQ', ' AMD', ' GNRC', ' CTRL'
-      ]
-    }
-  ]
+    ]
+  }
 ];
 
 //printing representative's information to the DOM
-for (let i = 0; i < tradeList.length; i++) {  
-  document.write(`${tradeList[i][0].name}: ${tradeList[i][2].party} representing ${tradeList[i][1].state}<br>Total trade volume: 
-  ${tradeList[i][4].tradeVolume}<br>Stock purchases:${tradeList[i][3].tradeType[0].purchase} *** Stock sales:
-  ${tradeList[i][3].tradeType[1].sale}<br>Top Traded Companies by Ticker: ${tradeList[i][5].topTenTickers}`);
-  
+tradeList.forEach(function(congressRep) {
+  document.write(`${congressRep.name}: ${congressRep.party} representing ${congressRep.state}<br>Total trade volume: 
+  ${congressRep.tradeVolume}<br>Stock purchases: ${congressRep.tradeType.purchase} *** Stock sales: 
+  ${congressRep.tradeType.sale}<br>Top Traded Companies by Ticker: ${congressRep.topTenTickers}`);
+
   //searching for less than 10 companies traded and noting that with a comment in the DOM
-  if (tradeList[i][5].topTenTickers.length < 10) {
+  if (congressRep.topTenTickers.length < 10) {
     document.write('<em> *Fewer companies traded overall </em>');
   }
-  
-  document.write('<br><br>')
-}
+  document.write('<br><br>');
+  }
+)
