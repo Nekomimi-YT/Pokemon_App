d22bc162e0d4fa72482442be28a974206fc1ee6b
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-06-15 13:10:32 +0200

    Add first attempt at fetching API in the app/ remove previous list to bottom comments

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,98 @@ money exchanged in both buy and sell transactions), and the top ten company tick
 // Current short list of representatives
 
 let tradeRepository = (function () {
+  let pokemonList = [];
+  let apiUrl = 'https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_transactions.json';
+
+//returns the entire tradeList
+  function getAll() {
+    return tradeList;
+  }
+
+//adds objects to tradeList
+  function add(congressRep) {
+    if (typeof congressRep !== 'object') {
+      return 'Additions must be added as objects';
+    }else if (Object.keys(congressRep) !== 'representative', 'state', 'party', 'tradeType', 'tradeVolume', 'topTickers') {
+      return 'Object keys invalid'
+    }else{
+      tradeList.push(congressRep);
+    }    
+  }
+
+//printing representative's names to the DOM in button form using 
+
+  function addListItem(congressRep) {
+    //link to <ul class = 'list'> in HTML and create <li>
+    let repList = document.querySelector('.list');
+    let listItem = document.createElement('li');
+    //create a new button element and style
+    let button = document.createElement('button');
+    button.innerText = (congressRep.name);
+    button.classList.add('button-style');
+    //add button to the page
+    listItem.appendChild(button);
+    repList.appendChild(listItem);
+    //when clicked, buttons log name of Congress Representative to the console
+    button.addEventListener('click', function () {
+      showDetails(congressRep); 
+    });  
+  }
+
+  function showDetails(congressRep) {
+    console.log(congressRep.name);
+  };
+
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {  //my aptUrl is already .json.  Do I need this step? 
+      json.results.forEach(function (item) {  //can I create a variable for json.results for an array?
+        congressRep = {
+          representative: item.representative  //getting the representative name from each item in json array
+        };
+        let distinct = [] //need to make sure not to duplicate names in the output, so trying to create a function for that
+          for (let i = 0; i < json.results.length; i++) {
+            if (json.results[i].representative not in distinct) {
+              add(congressRep);
+            }else{
+            distinct.push(json.results[i].representative)
+            }
+          }
+        });
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+    addListItem: addListItem,
+    loadList: loadList
+  };
+})();
+
+tradeRepository.loadList().then(function() {
+  tradeRepository.getAll().forEach(function(congressRep) {
+    tradeRepository.addListItem(congressRep);
+  });
+});
+
+
+
+/* keeping this code right now so I can review string literal templates
+document.write(`${congressRep.name}: ${congressRep.party} representing ${congressRep.state}<br>Total trade volume: 
+  ${congressRep.tradeVolume}<br>Stock purchases: ${congressRep.tradeType.purchase} *** Stock sales: 
+  ${congressRep.tradeType.sale}<br>Top Traded Companies by Ticker: ${congressRep.topTenTickers}`);
+
+//searching for less than 10 companies traded and noting that with a comment in the DOM
+  if (congressRep.topTenTickers.length < 10) {
+   document.write('<em> *Fewer companies traded overall </em>');
+  }
+  document.write('<br><br>');
+  
+  
   let tradeList = [
     {
       name: 'Hon. Virginia Foxx',
@@ -74,65 +166,4 @@ let tradeRepository = (function () {
         'MS', ' APPL', ' DG', ' BBH', ' CG', ' PYPL', ' SQ', ' AMD', ' GNRC', ' CTRL'
       ]
     }
-  ];
-
-//returns the entire tradeList
-  function getAll() {
-    return tradeList;
-  }
-
-//adds objects to tradeList
-  function add(congressRep) {
-    if (typeof congressRep !== 'object') {
-      return 'Additions must be added as objects';
-    }else if (Object.keys(congressRep) !== 'name', 'state', 'party', 'tradeType', 'tradeVolume', 'topTickers') {
-      return 'Object keys invalid'
-    }else{
-      tradeList.push(congressRep);
-    }    
-  }
-
-//printing representative's names to the DOM in button form using 
-
-  function addListItem(congressRep) {
-    //link to <ul class = 'list'> in HTML and create <li>
-    let repList = document.querySelector('.list');
-    let listItem = document.createElement('li');
-    //create a new button element and style
-    let button = document.createElement('button');
-    button.innerText = (congressRep.name);
-    button.classList.add('button-style');
-    //add button to the page
-    listItem.appendChild(button);
-    repList.appendChild(listItem);
-    //when clicked, buttons log name of Congress Representative to the console
-    button.addEventListener('click', function () {
-      showDetails(congressRep); 
-    });  
-  }
-
-  function showDetails(congressRep) {
-    console.log(congressRep.name);
-  };
-
-  return {
-    getAll: getAll,
-    add: add,
-    addListItem: addListItem
-  };
-})();
-
-tradeRepository.getAll().forEach(tradeRepository.addListItem);
-
-
-
-/* keeping this code right now so I can review string literal templates
-document.write(`${congressRep.name}: ${congressRep.party} representing ${congressRep.state}<br>Total trade volume: 
-  ${congressRep.tradeVolume}<br>Stock purchases: ${congressRep.tradeType.purchase} *** Stock sales: 
-  ${congressRep.tradeType.sale}<br>Top Traded Companies by Ticker: ${congressRep.topTenTickers}`);
-
-//searching for less than 10 companies traded and noting that with a comment in the DOM
-  if (congressRep.topTenTickers.length < 10) {
-   document.write('<em> *Fewer companies traded overall </em>');
-  }
-  document.write('<br><br>');*/
+  ];*/
