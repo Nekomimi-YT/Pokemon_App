8a8e5b5035b0034c432f8b5e629950a45fa266f3
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-06-30 16:13:42 +0200

    Update and clean comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
 /* 
-Using data from this pokemon App for this task:
+Using first 150 data targets from this pokemon App for this project:
 (https://pokeapi.co/api/v2/pokemon/?limit=150)
 
 */
 
-// Creating an IIFE to hold the Pokemon Repository and critical functions
+// Create an IIFE to hold the Pokemon Repository and critical functions
 
 let pokemonRepository = (function () {
   
-  //creating the Pokemon List from outside source
+  //Create the Pokemon List from outside API source
+  
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-  //fetches the pokemon list from the api
+  //Fetch the pokemon list from the API
+  
   function loadList() {
     return fetch(apiUrl).then(function (response) {
       return response.json();
@@ -29,12 +31,14 @@ let pokemonRepository = (function () {
     })
   }
 
-  //returns the entire pokemonList
+  //Return the entire pokemonList
+  
   function getAll() {
     return pokemonList;
   }
 
-  //adds objects to pokemonList
+  //Add objects to pokemonList
+  
   function add(pokemon) {
     if (
       typeof pokemon === 'object' &&
@@ -46,7 +50,8 @@ let pokemonRepository = (function () {
     }
   }
 
-//printing pokemon names to the DOM in button form using bootstrap
+//Print pokemon names to the DOM in button form using bootstrap
+  
   function addListItem(pokemon) {
       let pokemonList = document.querySelector('ul');
       pokemonList.classList.add('pokemonList');
@@ -54,7 +59,9 @@ let pokemonRepository = (function () {
       listItem.classList.add('list-group-item', 'col-sm-12', 'col-md-6', 'col-lg-3');
       let button = document.createElement('button');
       button.classList.add('btn', 'btn-light', 'btn-search');
+      
       //Capitalize first letter of Pokemon name and add as button text
+      
       button.innerText = (pokemon.name[0].toUpperCase() + pokemon.name.substring(1));
       button.setAttribute('data-toggle', 'modal');
       button.setAttribute('data-target', '.modal');
@@ -71,13 +78,16 @@ let pokemonRepository = (function () {
     });
   };
 
+  //Load additional details for display from URL item
+
   function loadDetails(item) {
     let url = item.detailsUrl;
     return fetch(url).then(function (response) {
       return response.json();
     }).then(function (details) {
-      // Now we add the details to the item
-      item.imageUrl = details.sprites.other.dream_world.front_default;
+      
+      // Add details to each pokemon item
+      item.imageUrl = details.sprites.other.dream_world.front_default; //details = linked page
       item.height = details.height;
       item.weight = details.weight;
       typeList = [];
@@ -99,28 +109,29 @@ let pokemonRepository = (function () {
     });
   }
   
-  //adding modal template to display pokemon data
+  //Add modal template to display pokemon data
+ 
   function showModal(item) {
     let modalBody = $('.modal-body');
     let modalTitle = $('.modal-title');
     let modalHeader = $('.modal-header');
     
-    //clear title and body elements from the modal
+    //Clear title and body elements from the modal
+   
     modalTitle.empty();
     modalBody.empty();
 
-    //create the name element in modal content
+    //Create the elements for modal content
     let nameElement = $('<h1>' + item.name[0].toUpperCase() + item.name.substring(1) + '</h1>');
-    //create the image in modal content
     let imageElement = $('<img class="modal-img">')
     imageElement.attr('src', item.imageUrl);
-    //create the height element for modal
     let heightElement = $('<p>' + 'Height: ' + item.height + '</p>');
-    //create the weight element for modal
     let weightElement = $('<p>' + 'Weight: ' + item.weight + '</p>');
     let typeElement = $('<p>' + 'Types: ' + item.types + '</p>');
     let abilityElement = $('<p>' + 'Abilities: ' + item.abilities + '</p>');
 
+    //Append all elements to the modal
+   
     modalTitle.append(nameElement);
     modalBody.append(imageElement);
     modalBody.append(heightElement);
@@ -130,7 +141,8 @@ let pokemonRepository = (function () {
 
   }
 
-  //Filter search input against button list
+  //Filter search input against the pokemon data list
+  
   function filterList() {
     let value = searchInput.val().toLowerCase();
   
@@ -156,8 +168,9 @@ let pokemonRepository = (function () {
 
 //Create a list of Pokemon name buttons and display on the screen. 
 //When clicked, each button pulls up a modal to display pokemon information
+
 pokemonRepository.loadList().then(function() {
-  // Data is loaded...
+  // Data is loaded...and then...
   pokemonRepository.getAll().forEach(function(pokemon){
     pokemonRepository.addListItem(pokemon);
   });
@@ -165,5 +178,6 @@ pokemonRepository.loadList().then(function() {
 
 //Searchbar functionality: filters displayed buttons by input  
 //Hides all buttons that don't match search input  
+
 let searchInput = $('input');
 searchInput.on('input', pokemonRepository.filterList);
