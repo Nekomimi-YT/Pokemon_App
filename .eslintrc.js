0d01d1ede1223db41a8844c001a52d170b30101c
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-07-01 17:30:48 +0200

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.export = { 
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'extends': 'eslint:recommended',
+    'parser': '@babel/eslint-parser',
+    'rules': {
+        'quotes': ['error', 'single']
+    }
+}
+
